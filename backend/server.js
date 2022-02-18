3cf15380a0c32d6c84e6dde88792af39d0845a19
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-02-18 23:06:22 +0600

    First Route

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/goals", (req, res) => {
+  // res.send("Get goals");
+  res.status(200).json({ message: "Get goals" });
+});
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
