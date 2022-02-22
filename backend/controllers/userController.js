c6ed44d33ce3ba5c8f1064523ac52663542caf98
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-02-22 18:02:04 +0600

    Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,41 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if ((!name, !email, !password)) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
   res.json({ message: "Register User" });
-};
+});
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
   res.json({ message: "Login User" });
 };
 
 // @desc    Get user data
 // @route   GET /api/users/me
 // @access  Private
-const getMe = (req, res) => {
+const getMe = async (req, res) => {
   res.json({ message: "User data display" });
 };
 
